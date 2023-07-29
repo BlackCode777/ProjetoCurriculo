e2ace98b0bd37b88b7f8dc5ae707d73cb38e9ff8
Anderson Martins <BlackCode77.1980@gmail.com>
2023-07-29 18:25:26 -0300

    [ Projeto_Curriculo ] - Início do projeto - v.0.0

---
+++ Curriculo_Spring/src/main/java/com/spring_curriculo_backend/Curriculo_Spring/CurriculoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.spring_curriculo_backend.Curriculo_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculoSpringApplication.class, args);
+	}
+
+}
---
+++ Curriculo_Spring/src/test/java/com/spring_curriculo_backend/Curriculo_Spring/CurriculoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_curriculo_backend.Curriculo_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
